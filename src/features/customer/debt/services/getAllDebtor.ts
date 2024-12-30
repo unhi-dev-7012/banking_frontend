a125da6b2428adf49b642080fa7cb2a4fd0ebe1e
unhi-dev-7012 <fanhi11211@gmail.com>
2024-12-30 18:27:35 +0000

    chore: clear console log

--- src/features/customer/debt/services/getAllDebtor.ts
+++ src/features/customer/debt/services/getAllDebtor.ts
@@ -3,7 +3,6 @@ import api from "@utils/api";
 
 export const getAllDebtor = async () => {
   const { data } = await api.get(`/api/customer/v1/debt/list/debtors`);
-  console.log("data", data);
 
   return data;
 };
