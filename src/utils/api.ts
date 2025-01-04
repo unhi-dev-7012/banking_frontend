0b944702f2ad7b0f782ea26fb8426223dbf40d00
nkhy-dev-3112 <nkhy.dev@gmail.com>
2025-01-04 14:47:21 +0700

    add: customer dashboard screen

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 10000,
+  timeout: 20000,
   headers: {
     "Content-Type": "application/json",
   },
